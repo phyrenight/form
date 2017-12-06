8f7e73c7f9bf17c571df597a4ffec8c069b71072
Marc Preston <phyrenight@gmail.com>
2017-12-06 01:16:11 -0500

    add results page

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from config import secret_key
 from questionaire_db import Answers, session as db_session
 from forms import WebForm
@@ -7,15 +7,24 @@ app = Flask(__name__)
 app.secret_key = secret_key
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     form = WebForm()
+    if request.method == 'POST':
+    	return redirect(url_for('results'))
+
     return render_template('home.html', form=form)
 
-@app.route('/')
-def summary():
-    # db_session.query
-    return render_template('summary')
+@app.route('/results')
+def results():
+    pepsi_count = db_session.query(Answers).filter(Answers.question_one == 'Pepsi').count()
+    coke_count = db_session.query(Answers).filter(Answers.question_one == 'Coke').count()
+    print coke_count
+    print pepsi_count
+    return render_template(
+    	'results.html',
+        coke_count=coke_count,
+        pepsi_count=pepsi_count)
 
 if __name__ == "__main__":
     app.run(debug=True)
